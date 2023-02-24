8a0316c3f2729fca0ea3fd98b57b21d429e08b22
holysmokes48 <snssatb@gmail.com>
2023-02-23 22:17:41 -0500

    adding shared, component, and service folder for angular project

--- employee-app/src/app/app.module.ts
+++ employee-app/src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainappComponent } from './component/mainapp/mainapp.component';
+import { HeaderComponent } from './component/header/header.component';
+import {HttpClientModule} from '@angular/common/http';
+import { LoginComponent } from './component/login/login.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainappComponent,
+    HeaderComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ employee-app/src/app/shared/app.routing.ts
@@ -0,0 +1,6 @@
+import { RouterModule } from "@angular/router";
+
+export const AppRouting = RouterModule.forRoot([
+
+    {path:'', redirectTo:'mainapp', pathMatch:'full'}
+])
