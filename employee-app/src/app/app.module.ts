6a5eaa4898d212c13bca5a3391a4fe02d3a64cb3
holysmokes48 <snssatb@gmail.com>
2023-02-23 22:26:26 -0500

    added footer component and changed fontAwesome reference code

--- employee-app/src/app/app.module.ts
+++ employee-app/src/app/app.module.ts
@@ -5,14 +5,16 @@ import { AppComponent } from './app.component';
 import { MainappComponent } from './component/mainapp/mainapp.component';
 import { HeaderComponent } from './component/header/header.component';
 import {HttpClientModule} from '@angular/common/http';
-import { LoginComponent } from './component/login/login.component'
+import { LoginComponent } from './component/login/login.component';
+import { FooterComponent } from './component/footer/footer.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     MainappComponent,
     HeaderComponent,
-    LoginComponent
+    LoginComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule, HttpClientModule
